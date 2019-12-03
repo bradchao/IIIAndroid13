4b8a48e5861b7fb462c34926e0131924a86d199f
brad <brad@brad.tw>
2019-12-03 14:57:41 +0800

    2

--- app/src/main/java/com/example/iiiandroid13/MainActivity.java
+++ app/src/main/java/com/example/iiiandroid13/MainActivity.java
@@ -5,18 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
+    private EditText n;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        n = findViewById(R.id.n);
         webView = findViewById(R.id.webView);
         initWebView();
     }
@@ -41,4 +45,8 @@ public class MainActivity extends AppCompatActivity {
         return super.onKeyDown(keyCode, event);
     }
 
+    public void test1(View view) {
+        webView.loadUrl(
+                "javascript:test2(" + n.getText().toString() + ")");
+    }
 }
