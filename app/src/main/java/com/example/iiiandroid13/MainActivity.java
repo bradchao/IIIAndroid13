3e95187d5252437effd74e5464fb850986205ce2
brad <brad@brad.tw>
2019-12-03 16:25:34 +0800

    5

--- app/src/main/java/com/example/iiiandroid13/MainActivity.java
+++ app/src/main/java/com/example/iiiandroid13/MainActivity.java
@@ -6,7 +6,11 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -20,6 +24,8 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     private WebView webView;
     private EditText n;
+    private LocationManager lmgr;
+    private MyListener myListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,11 +54,54 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init(){
+        lmgr = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+
+
         n = findViewById(R.id.n);
         webView = findViewById(R.id.webView);
         initWebView();
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        myListener = new MyListener();
+        lmgr.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,0,0,myListener);
+    }
+
+    private class MyListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location location) {
+            double lat = location.getLatitude();
+            double lng = location.getLongitude();
+            Log.v("brad", lat + " x " + lng);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        lmgr.removeUpdates(myListener);
+
+    }
 
     private void initWebView(){
         WebViewClient webViewClient = new WebViewClient();
