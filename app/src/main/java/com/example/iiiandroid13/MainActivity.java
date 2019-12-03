c596f5bff4fad91007b4fe7743fdf815665bfc74
brad <brad@brad.tw>
2019-12-03 15:47:53 +0800

    4

--- app/src/main/java/com/example/iiiandroid13/MainActivity.java
+++ app/src/main/java/com/example/iiiandroid13/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.iiiandroid13;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -20,11 +25,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    123);
+        }else{
+            init();
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            init();
+        }else{
+            finish();
+        }
+    }
+
+    private void init(){
         n = findViewById(R.id.n);
         webView = findViewById(R.id.webView);
         initWebView();
     }
+
 
     private void initWebView(){
         WebViewClient webViewClient = new WebViewClient();
@@ -35,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
         webView.addJavascriptInterface(new MyJS(), "brad");
 
-        webView.loadUrl("file:///android_asset/brad.html");
+        webView.loadUrl("file:///android_asset/map.html");
     }
 
     @Override
